45986440fe1bd12ce00acfb6624e919261065c8a
Glenn <Glenn.tetteh@mogelijk.nl>
2025-06-10 14:21:40 +0200

    Terugbelverzoek toegevoegd

---
+++ cypress/e2e/Mijn.mogelijk/Terugbel.cy.js
@@ -0,0 +1,19 @@
+describe('Terugbelverzoek', () => {
+  it('Terugbelverzoek indienen', () => {
+
+
+    cy.MMlogin('mirra@mirra.nl','Bbxa3859?')
+
+    cy.title().should('eq', 'Actueel aanbod | Mogelijk Vastgoedfinancieringen')
+
+cy.get('[data-test-id="contact-button"]').click()
+cy.get('[data-test-id="dynamic-form-button"]').click()
+
+cy.contains('Hartelijk dank')
+      .should('be.visible')
+
+cy.get('[data-test-id="modal-close-button"]').click()
+
+  })
+
+})
